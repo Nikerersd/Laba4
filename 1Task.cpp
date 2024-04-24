0116306e71ed8d19d4699b92e2c7dd6cc99b2055
Nikerersd <nikrudk0@yandex.ru>
2024-04-25 02:30:12 +0700

    1TaskNew

--- 1Task.cpp
+++ 1Task.cpp
@@ -20,7 +20,7 @@ double g(double x) {
     else return 1.0/log(x+1);
 }
 
-void bisectionMethod(double a, double b, double eps, vector<tuple<double, double, double, double>>& bisection) {
+void bisectionMethod(double a, double b, double eps, vector<tuple<double, double, double, double>>& bisection, double& res) {
     int N = 0;
     while ((b - a) >= eps && N < 100) {
         double c = (a + b) / 2;
@@ -35,10 +35,11 @@ void bisectionMethod(double a, double b, double eps, vector<tuple<double, double
             a = c;
         }
         N++;
+        res = c;
     }
 }
 
-void NewtonMethod(double x0, double eps, vector<tuple<double, double, double, double>>& newton) {
+void NewtonMethod(double x0, double eps, vector<tuple<double, double, double, double>>& newton, double& res) {
     int N = 0;
     while (abs(f(x0)) >= eps && N < 100) {
         double x1 = x0 - f(x0) / df(x0);
@@ -50,10 +51,11 @@ void NewtonMethod(double x0, double eps, vector<tuple<double, double, double, do
         }
         x0 = x1;
         N++;
+        res = x0;
     }
 }
 
-void simpleMethod(double x0, double eps, vector<tuple<double, double, double, double>>& simple) {
+void simpleMethod(double x0, double eps, vector<tuple<double, double, double, double>>& simple, double& res) {
     int N = 0;
     while (abs(g(x0)) >= eps && N < 100) {
         double x1 = g(x0);
@@ -63,11 +65,13 @@ void simpleMethod(double x0, double eps, vector<tuple<double, double, double, do
         simple.push_back({N, x0, x1, abs(x1 - x0)});
         x0 = x1;
         N++;
+        res = x0;
     }
 }
 
 int main() {
     double a = -1, b = 2, eps = 0.0001;
+    double res;
     vector<tuple<double, double, double, double>> bisection;
     vector<tuple<double, double, double, double>> newton;
     vector<tuple<double, double, double, double>> simple;
@@ -78,27 +82,33 @@ int main() {
         return 1;
     }
     //Первый корень
-    bisectionMethod(a, b, eps, bisection);
+    bisectionMethod(a, b, eps, bisection, res);
     outfile << "Метод половинного деления(первый корень):" << endl;
     outfile << "N" << setw(20) << "a" << setw(20) << "b" << setw(20) << "b - a" << endl;
     for (tuple<double, double, double, double> i : bisection) {
         outfile << get<0>(i) << setw(20) << get<1>(i) << setw(20) << get<2>(i) << setw(20) << get<3>(i) << endl;
     }
     outfile << endl;
+    outfile << "Корень: " << res << endl;
+    outfile << endl;
     double x0 = -0.5;
-    NewtonMethod(x0, eps, newton);
+    NewtonMethod(x0, eps, newton, res);
     outfile << "Метод Ньютона(первый корень):" << endl;
     outfile << "N" << setw(20) << "x0" << setw(20) << "x1" << setw(20) << "x1 - x0" << endl;
     for (tuple<double, double, double, double> i : newton) {
         outfile << get<0>(i) << setw(20) << get<1>(i) << setw(20) << get<2>(i) << setw(20) << get<3>(i) << endl;
     }
     outfile << endl;
-    simpleMethod(x0, eps, simple);
+    outfile << "Корень: " << res << endl;
+    outfile << endl;
+    simpleMethod(x0, eps, simple, res);
     outfile << "Метод простых итераций(первый корень):" << endl;
     outfile << "N" << setw(20) << "x0" << setw(20) << "x1" << setw(20) << "x1 - x0" << endl;
     for (tuple<double, double, double, double> i : simple) {
         outfile << get<0>(i) << setw(20) << get<1>(i) << setw(20) << get<2>(i) << setw(20) << get<3>(i) << endl;
     }
+    outfile << endl;
+    outfile << "Корень: " << res << endl;
     outfile << endl;
     outfile << "--------------------------------------------------------------" << endl;
     //Второй корень
@@ -106,27 +116,32 @@ int main() {
     newton.clear();
     simple.clear();
     a = 0, b = 2;
-    bisectionMethod(a, b, eps, bisection);
+    bisectionMethod(a, b, eps, bisection, res);
     outfile << "Метод половинного деления(Второй корень):" << endl;
     outfile << "N" << setw(20) << "a" << setw(20) << "b" << setw(20) << "b - a" << endl;
     for (tuple<double, double, double, double> i : bisection) {
         outfile << get<0>(i) << setw(20) << get<1>(i) << setw(20) << get<2>(i) << setw(20) << get<3>(i) << endl;
     }
     outfile << endl;
+    outfile << "Корень: " << res << endl;
+    outfile << endl;
     x0 = 0.5;
-    NewtonMethod(x0, eps, newton);
+    NewtonMethod(x0, eps, newton, res);
     outfile << "Метод Ньютона(Второй корень):" << endl;
     outfile << "N" << setw(20) << "x0" << setw(20) << "x1" << setw(20) << "x1 - x0" << endl;
     for (tuple<double, double, double, double> i : newton) {
         outfile << get<0>(i) << setw(20) << get<1>(i) << setw(20) << get<2>(i) << setw(20) << get<3>(i) << endl;
     }
     outfile << endl;
-    simpleMethod(x0, eps, simple);
+    outfile << "Корень: " << res << endl;
+    outfile << endl;
+    simpleMethod(x0, eps, simple, res);
     outfile << "Метод простых итераций(Второй корень):" << endl;
     outfile << "N" << setw(20) << "x0" << setw(20) << "x1" << setw(20) << "x1 - x0" << endl;
     for (tuple<double, double, double, double> i : simple) {
         outfile << get<0>(i) << setw(20) << get<1>(i) << setw(20) << get<2>(i) << setw(20) << get<3>(i) << endl;
     }
     outfile << endl;
+    outfile << "Корень: " << res << endl;
     return 0;
 }
